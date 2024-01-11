9a999fbc31652782a32b5d21053dd385089ae3bf
techdev-VIK <soodvikartan@gmail.com>
2024-01-11 18:11:35 +0530

    static structure done

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -3,9 +3,13 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.calculator}>
-      <input id="display" type="text" />
-      <div id="buttons-container">
-        <button>C</button>
+      <input className={styles.display} type="text" />
+      <div className={styles.buttonsContainer}>
+        <button className={styles.button}>C</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>3</button>
+        <button className={styles.button}>4</button>
       </div>
     </div>
   );
